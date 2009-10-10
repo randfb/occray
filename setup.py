7c8c8a52fc798a2ca7f44f822468cb19cff0bb56
sebastien.ramage <sebastien.ramage@ab136e48-ace9-11de-b6f4-e1be98fab775>
2009-10-10 14:21:54 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! -*- coding: utf-8 -*-
+
+
+from distutils.core import setup
+
+setup(name='occray',
+      version='0.1',
+      description='PythonOCC renderer using Yafaray',
+      author='Sébastien Ramage',
+      author_email='',
+      url='http://code.google.com/p/occray/',
+      packages=['occray'],
+     )
